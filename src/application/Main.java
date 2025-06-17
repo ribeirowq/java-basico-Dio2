b4e40f2f212e501b32e0e53b90cf6d037ab1551f
Thiago Lima <Ribeiro7380@gmail.com>
2025-06-17 18:39:13 -0400

    firts commit

---
+++ src/application/Main.java
@@ -0,0 +1,38 @@
+package application;
+
+
+import exceptions.ParametrosInvalidosException;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro:");
+        int parametroUm = sc.nextInt();
+        System.out.println("Digite o segundo parâmetro:");
+        int parametroDois = sc.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+
+        }catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        sc.close();
+    }
+
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        } else {
+            int contagem = parametroDois - parametroUm;
+            for (int i = 0; i <= contagem; i++){
+                System.out.println(i);
+            }
+        }
+    }
+}
